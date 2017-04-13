e3d2ebc99c992bcb0fdc06338c8f3da621bf3eb6
Arthur Ramos <ramos.oceano@gmail.com>
2017-04-13 12:51:03 -0300

    first part

---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+  validates :description, presence: :true
+  validates :cocktail, presence: :true
+  validates :name, presence: :true
+  validates inclusion: { in: [:cocktail, :ingredient], uniqueness: :true}
+
+end
